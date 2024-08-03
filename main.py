4e37baedfb73bce463de9e31221baa9fc377306a
thefeymesaleng <thefeymesaleng@gmail.com>
2024-08-04 03:51:55 +0800

    add todo

--- main.py
+++ main.py
@@ -13,17 +13,19 @@ APP_TEMPLATE_PATH = "app_templates"
 WIFI_FILE = "wifi.json"
 WIFI_MAX_ATTEMPTS = 3
 
+
 def machine_reset():
     utime.sleep(1)
     print("Resetting...")
     machine.reset()
 
+
 def setup_mode():
     print("Entering setup mode...")
-    
+
     def ap_index(request):
         if request.headers.get("host").lower() != AP_DOMAIN.lower():
-            return render_template(f"{AP_TEMPLATE_PATH}/redirect.html", domain = AP_DOMAIN.lower())
+            return render_template(f"{AP_TEMPLATE_PATH}/redirect.html", domain=AP_DOMAIN.lower())
 
         return render_template(f"{AP_TEMPLATE_PATH}/index.html")
 
@@ -36,23 +38,25 @@ def setup_mode():
 
         # Reboot from new thread after we have responded to the user.
         _thread.start_new_thread(machine_reset, ())
-        return render_template(f"{AP_TEMPLATE_PATH}/configured.html", ssid = request.form["ssid"])
-        
+        return render_template(f"{AP_TEMPLATE_PATH}/configured.html", ssid=request.form["ssid"])
+
     def ap_catch_all(request):
         if request.headers.get("host") != AP_DOMAIN:
-            return render_template(f"{AP_TEMPLATE_PATH}/redirect.html", domain = AP_DOMAIN)
+            return render_template(f"{AP_TEMPLATE_PATH}/redirect.html", domain=AP_DOMAIN)
 
         return "Not found.", 404
 
-    server.add_route("/", handler = ap_index, methods = ["GET"])
-    server.add_route("/configure", handler = ap_configure, methods = ["POST"])
+    server.add_route("/", handler=ap_index, methods=["GET"])
+    server.add_route("/configure", handler=ap_configure, methods=["POST"])
     server.set_callback(ap_catch_all)
 
     ap = access_point(AP_NAME)
     ip = ap.ifconfig()[0]
     dns.run_catchall(ip)
 
+
 def application_mode():
+    # TODO: add app logic here
     print("Entering application mode.")
     onboard_led = machine.Pin("LED", machine.Pin.OUT)
 
@@ -62,7 +66,7 @@ def application_mode():
     def app_toggle_led(request):
         onboard_led.toggle()
         return "OK"
-    
+
     def app_get_temperature(request):
         # Not particularly reliable but uses built in hardware.
         # Demos how to incorporate senasor data into this application.
@@ -74,24 +78,26 @@ def application_mode():
         reading = sensor_temp.read_u16() * (3.3 / (65535))
         temperature = 27 - (reading - 0.706)/0.001721
         return f"{round(temperature, 1)}"
-    
+
     def app_reset(request):
         # Deleting the WIFI configuration file will cause the device to reboot as
         # the access point and request new configuration.
         os.remove(WIFI_FILE)
         # Reboot from new thread after we have responded to the user.
         _thread.start_new_thread(machine_reset, ())
-        return render_template(f"{APP_TEMPLATE_PATH}/reset.html", access_point_ssid = AP_NAME)
+        return render_template(f"{APP_TEMPLATE_PATH}/reset.html", access_point_ssid=AP_NAME)
 
     def app_catch_all(request):
         return "Not found.", 404
 
-    server.add_route("/", handler = app_index, methods = ["GET"])
-    server.add_route("/toggle", handler = app_toggle_led, methods = ["GET"])
-    server.add_route("/temperature", handler = app_get_temperature, methods = ["GET"])
-    server.add_route("/reset", handler = app_reset, methods = ["GET"])
+    server.add_route("/", handler=app_index, methods=["GET"])
+    server.add_route("/toggle", handler=app_toggle_led, methods=["GET"])
+    server.add_route(
+        "/temperature", handler=app_get_temperature, methods=["GET"])
+    server.add_route("/reset", handler=app_reset, methods=["GET"])
     # Add other routes for your application...
     server.set_callback(app_catch_all)
+
 
 # Figure out which mode to start up in...
 try:
@@ -101,20 +107,21 @@ try:
     with open(WIFI_FILE) as f:
         wifi_current_attempt = 1
         wifi_credentials = json.load(f)
-        
+
         while (wifi_current_attempt < WIFI_MAX_ATTEMPTS):
-            ip_address = connect_to_wifi(wifi_credentials["ssid"], wifi_credentials["password"])
+            ip_address = connect_to_wifi(
+                wifi_credentials["ssid"], wifi_credentials["password"])
 
             if is_connected_to_wifi():
                 print(f"Connected to wifi, IP address {ip_address}")
                 break
             else:
                 wifi_current_attempt += 1
-                
+
         if is_connected_to_wifi():
             application_mode()
         else:
-            
+
             # Bad configuration, delete the credentials file, reboot
             # into setup mode to get new credentials from the user.
             print("Bad wifi connection!")
@@ -123,7 +130,7 @@ try:
             machine_reset()
 
 except Exception:
-    # Either no wifi configuration file found, or something went wrong, 
+    # Either no wifi configuration file found, or something went wrong,
     # so go into setup mode.
     setup_mode()
 
